eb3e38c3de5ef9930eee815608f2e389b4dcf28f
kendydo02 <kendydo02@gmail.com>
2024-08-17 00:26:08 +0700

    Add files via upload

---
+++ Swisstronik_ERC20/scripts/mint.js
@@ -0,0 +1,42 @@
+const hre = require("hardhat");
+
+const { encryptDataField } = require("@swisstronik/utils");  
+
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x0c2C1377824aDda5FF054A7Cc7b2cF3D427808c9";  
+
+  const [signer] = await hre.ethers.getSigners();
+
+  const contractFactory = await hre.ethers.getContractFactory("Swisstronik"); 
+  const contract = contractFactory.attach(contractAddress);
+
+  const functionName = "mint";
+  const mintTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName),
+    0
+  );
+
+  await mintTx.wait();
+
+  console.log("Mint transaction receipt: ", mintTx);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
